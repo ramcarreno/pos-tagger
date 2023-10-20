717c30354b5d92b3f71cc9b899f6c058e79cc5d5
Rubén <rubgij@gmail.com>
2023-10-20 19:15:18 +0200

    Update pos_tagger.py

--- pos_tagger.py
+++ pos_tagger.py
@@ -14,35 +14,22 @@ def calculate_A(corpus: list[list[tuple]]):
     Returns
     -------
     A : defaultdict[defaultdict]
-        Transition matrix of the corpus. Contains the Log2 of the probability.
-
-    vocab : set
-        Vocabulary with all the training tokens.
-
-    observations : set
-        Contains all the observations found in the corpus.
+        Transition matrix of the corpus. Contains the Log2 of the probability. log(p) of unknown = -np.inf
     """
     count = defaultdict(lambda: defaultdict(lambda: 0))
-    vocab = set(["UNK"])
-    observations = set()
 
     for sentence in corpus:
-        vocab.add(sentence[0][0])
-        observations.add(sentence[0][1])
         for prev_o, next_o in zip(sentence[:-1], sentence[1:]):
             count[prev_o[1]][next_o[1]] += 1
-            vocab.add(next_o[0])
-            observations.add(next_o[1])
-    
+
+    # REVISAR EL CODIGO, ESTA AL REVES, no debe dividirse entre ese total        
     A = defaultdict(lambda: defaultdict(lambda: -np.inf))
-    for prev_o, next_os in count.items():
-        total = sum(next_os.values())
-        for next_o, freq in next_os.items():
+    for prev_o, next_posibilities in count.items():
+        total = sum(next_posibilities.values())
+        for next_o, freq in next_posibilities.items():
             A[prev_o][next_o] = np.log2(freq/total)
     
-    return A, vocab, observations
-
-
+    return A
 
 
 def calculate_B(corpus: list[tuple], unk_threshold=3):
@@ -53,6 +40,7 @@ def calculate_B(corpus: list[tuple], unk_threshold=3):
     ----------
     corpus : list[tuple]
         All the training senteces as a list of tuples.
+    
     unk_threshold : int
         Threshold to consider if a word is UNK or not.
 
@@ -60,12 +48,41 @@ def calculate_B(corpus: list[tuple], unk_threshold=3):
     -------
     B : ????
         Matrix...
+    
+    vocab : set
+        Vocabulary with all the training tokens.
+
+    observations : set
+        Contains all the observations found in the corpus.
     """
-    pass
+    count = defaultdict(lambda: defaultdict(lambda: 0))
+    word_freq = defaultdict(lambda: 0)
+    vocab = set(["UNK"])
+    observations = set()
+
+    for sentence in corpus:
+        for word, observation in sentence:
+            count[word][observation] += 1
+            observations.add(observation)
+
+    B = defaultdict(lambda: defaultdict(lambda: -np.inf))
+    for word, freqs in 
+
+    return B, vocab, observations
 
 
-def calculate_PI(corpus: list[tuple], unk_threshold=3):
-    pass
+def calculate_PI(corpus: list[tuple]):
+    count = defaultdict(lambda: 0)
+    for sentence in corpus:
+        first_observation = sentence[0][1]
+        count[first_observation] += 1
+    
+    PI = defaultdict(lambda: -np.inf)
+    total = sum(count.values())
+    for observation, freq in count.items():
+        PI[observation] = np.log2(freq/total)
+    
+    return PI
 
 
 def predict(sentence: list, A, B, PI, vocab)-> list:
