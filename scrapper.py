d746ebc7a3423564290a4b54a42486b7bb182c1a
mariagabv <magarcaa@ucm.es>
2023-10-22 15:41:23 +0200

    scrapper.py is now commented

--- scrapper.py
+++ scrapper.py
@@ -1,20 +1,33 @@
 def parse_conllu_file(filepath):
-    sentences = []
-    sentence = []
-
+    
+    """ 
+    Parse a CoNLL-U format file and return list[list[tuple]] of word and pos-tag
+    
+    to be used as part of corpus preparation for the file pos_tagger.py 
+    
+    """
+    
+    single_sentence = []
+    all_sentences = []
+    
+    # deletes whitespaces and skips comments
     with open(filepath, 'r', encoding='utf-8') as conllu_file:
         for line in conllu_file:
             line = line.strip()
             if line.startswith("#"):
                 continue
-
+            
+            # extracts word and pos-tag creating a nested list of sentences
             columns = line.split("\t")
-
             if len(columns) != 1:
-                sentence.append((columns[1], columns[3]))
+                single_sentence.append((columns[1], columns[3]))
             else:
-                sentences.append(sentence)
-                sentence = []
+                all_sentences.append(single_sentence)
+                single_sentence = []
+    
+    return all_sentences
+    
 
-    return sentences
+print(parse_conllu_file("C:\\Users\\usuario\\Desktop\\pos-tagger\\datasets\\en_eslspok-ud-train.conllu"))
+
 
