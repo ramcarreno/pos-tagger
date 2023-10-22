8b33d03296858a3ede36a7f47b42d3763e8617b9
Rubén <rubgij@gmail.com>
2023-10-23 00:05:39 +0200

    Update pos_tagger.py

--- pos_tagger.py
+++ pos_tagger.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def calculate_A(corpus: list[list[tuple]]):
     """ 
-    Calculates the transition matrix, the corpus vocabulary, and the observations.
+    Calculates the transition matrix, the corpus vocabulary, and the states.
     
     Parameters
     ----------
@@ -24,20 +24,19 @@ def calculate_A(corpus: list[list[tuple]]):
         for prev, actual in zip(sentence[:-1], sentence[1:]):
             count[prev[1]][actual[1]] += 1
 
-    # REVISAR EL CODIGO, ESTA AL REVES, no debe dividirse entre ese total        
     A = defaultdict(lambda: defaultdict(lambda: -np.inf))
-    for prev_o, act_posibilities in count.items():
+    for prev_state, act_posibilities in count.items():
         total = sum(act_posibilities.values())
-        for actual_o, freq in act_posibilities.items():
-            A[prev_o][actual_o] = np.log2(freq/total)
+        for actual_state, freq in act_posibilities.items():
+            A[prev_state][actual_state] = np.log2(freq/total)
     
     return A
 
 
-def test_A(A, observations, epsilon=0.0000001):
-    for i in observations:
+def test_A(A, states, epsilon=0.0000001):
+    for i in states:
         suma = 0
-        for j in observations:
+        for j in states:
             suma += np.exp2(A[i][j])
         if abs(suma-1)>epsilon:
             print(f"ERROR: {i}. SUM: {suma} should be 1.")
@@ -58,44 +57,43 @@ def calculate_B(corpus: list[list[tuple]], unk_threshold=3):
 
     Returns
     -------
-    B : ????
-        Emission probabilities matrix...
+    B : defaultdict[defaultdict]
+        Emission probabilities matrix of the given corpus. Contains the Log2 of the probability. log(p) of unknown = -np.inf
     
     vocab : set
         Vocabulary with all the training tokens.
 
-    observations : set
-        Contains all the observations found in the corpus.
+    states : set
+        Contains all the states found in the corpus.
     """
     count = defaultdict(lambda: defaultdict(lambda: 0))
     word_freq = defaultdict(lambda: 0)
     vocab = set(["UNK"])
-    observations = set()
+    states = set()
 
     for sentence in corpus:
-        for word, observation in sentence:
+        for word, state in sentence:
             word_freq[word] += 1
-            observations.add(observation)
+            states.add(state)
     
     for word, freq in word_freq.items():
         if freq > unk_threshold:
             vocab.add(word)
     
     for sentence in corpus:
-        for word, observation in sentence:
+        for word, state in sentence:
             if word in vocab:
-                count[word][observation] += 1
+                count[word][state] += 1
             else:
-                count["UNK"][observation] += 1
+                count["UNK"][state] += 1
     
-
     B = defaultdict(lambda: defaultdict(lambda: -np.inf))
     for word, posibilities in count.items():
         total = sum(posibilities.values())
-        for observation, freq in posibilities.items():
-            B[word][observation] = np.log2(freq/total)
+        for state, freq in posibilities.items():
+            B[word][state] = np.log2(freq/total)
 
-    return B, vocab, observations
+    return B, vocab, states
 
 
 def test_B(B, vocab, epsilon=0.0000001):
@@ -104,16 +102,29 @@ def test_B(B, vocab, epsilon=0.0000001):
             print(f"ERROR: {word}. SUM: {abs(sum(B[word].values())-1)} should be 1.")
 
 
-def calculate_PI(corpus: list[tuple]):
+def calculate_PI(corpus: list[list[tuple]]):
+    """
+    Calculates the probability of being in the start of a sentence every possible state.
+
+    Parameters
+    ----------
+    corpus : list[list[tuple]]
+        All the training senteces as a list of tuples.
+
+    Returns
+    -------
+    PI : ????
+        ...
+    """
     count = defaultdict(lambda: 0)
     for sentence in corpus:
-        first_observation = sentence[0][1]
-        count[first_observation] += 1
+        first_state = sentence[0][1]
+        count[first_state] += 1
     
     PI = defaultdict(lambda: -np.inf)
     total = sum(count.values())
-    for observation, freq in count.items():
-        PI[observation] = np.log2(freq/total)
+    for state, freq in count.items():
+        PI[state] = np.log2(freq/total)
     
     return PI
         
