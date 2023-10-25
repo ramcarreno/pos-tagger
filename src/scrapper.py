e1d8e3639fad1a5e69ea652379941fe82be984b2
Maria <mribaltaalbado@gmail.com>
2023-10-25 12:37:41 +0200

    Modify structure of repo

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,22 +1,21 @@
 def parse_conllu_file(filepath):
-    
-    """ 
-    Parse a CoNLL-U format file and return list[list[tuple]] of word and pos-tag
-    
-    to be used as part of corpus preparation for the file pos_tagger.py 
-    
     """
-    
+    Parse a CoNLL-U format file and return list[list[tuple]] of word and pos-tag
+
+    to be used as part of corpus preparation for the file pos_tagger.py
+
+    """
+
     single_sentence = []
     all_sentences = []
-    
+
     # lowcase, delete whitespaces and skips comments
-    with open(filepath, 'r', encoding='utf-8') as conllu_file:
+    with open(filepath, "r", encoding="utf-8") as conllu_file:
         for line in conllu_file:
             line = line.lower().strip()
             if line.startswith("#"):
                 continue
-            
+
             # extract word and pos-tag creating a nested list of sentences
             columns = line.split("\t")
             if len(columns) != 1:
@@ -24,8 +23,5 @@ def parse_conllu_file(filepath):
             else:
                 all_sentences.append(single_sentence)
                 single_sentence = []
-    
+
     return all_sentences
-    
-
-
